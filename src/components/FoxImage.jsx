db372f2f0758bf928eebfe9f84cb776a6d40e43d
Ben Goddard <goddard.ben04@gmail.com>
2025-11-11 10:17:03 -0700

    Fetch fox image on mount using useEffect

--- src/components/FoxImage.jsx
+++ src/components/FoxImage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import foxLogo from '../assets/fox-logo.png'
 
 const API_URL = "https://randomfox.ca/floof/"
@@ -6,6 +6,16 @@ const API_URL = "https://randomfox.ca/floof/"
 function FoxImage() {
   const [image, setImage] = useState(foxLogo)
   
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => {
+        if(!response.ok) { thrown new Error("Failed to fetch image"); }
+        return response.json();
+      })
+      .then(data => setImage(data.image))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div>
       <p>Learn more about us!</p>
